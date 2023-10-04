079331de3e746f5af9b36cc06c3a3e6167d8b67b
RossDooney <rossdooney@gmail.com>
2023-10-04 13:23:50 +0100

    http request added

--- internal/pokeapi/location_area_resq.go
+++ internal/pokeapi/location_area_resq.go
@@ -1,5 +1,32 @@
 package pokeapi
 
+import (
+	"net/http"
+	"fmt"
+	"os"
+)
+
 func (c *Client) ListLocationAreas() (LocationAreaResp, error){
-	endpoint := "location-area"
+	endpoint := "/location-area"
+	fullURL := baseURL + endpoint
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil{
+		return LocationAreaResp{}, err
+	}
+	
+	resp, err := c.httpClient.Do(req)
+
+	if err != nil{
+		return LocationAreaResp{}, err
+	}
+
+	defer resp.body.Close()
+
+	if resp.StatusCode > 399{
+		return LocationAreaResp{}, fmt.Println("Bad Status code %v", resp.StatusCode)
+	}
+
+	io.ReadAll(resp.body)
+
 }
