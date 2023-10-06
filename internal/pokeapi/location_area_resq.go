8d4ce51a653ed8bd8aadd21637debe774794d1fb
Ross <rossdooney@gmail.com>
2023-10-06 19:34:30 +0100

    location struct added

--- internal/pokeapi/location_area_resq.go
+++ internal/pokeapi/location_area_resq.go
@@ -64,3 +64,57 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreaResp, error) {
 	return locationAreaResp, nil
 
 }
+
+func (c *Client) GetLocationAreas(locationAreaName string) (LocationArea, error) {
+	endpoint := "/location-area" + locationAreaName
+	fullURL := baseURL + endpoint
+
+	dat, ok := c.cache.Get(fullURL)
+
+	if ok {
+		fmt.Println("cache hit")
+		locationArea := LocationArea{}
+		err := json.Unmarshal(dat, &locationArea)
+		if err != nil {
+			return LocationArea{}, err
+		}
+
+		return LocationArea{}, nil
+
+	}
+	fmt.Println("cache miss")
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+
+	defer resp.Body.Close()
+
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 399 {
+		return LocationArea{}, fmt.Errorf("Bad status codeL: %v", resp.StatusCode)
+	}
+
+	dat, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	locationArea := LocationAreaResp{}
+	err = json.Unmarshal(dat, &locationArea)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	c.cache.Add(fullURL, dat)
+	fmt.Println("cache saved")
+	return LocationArea{}, nil
+
+}
